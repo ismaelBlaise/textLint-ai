f4a01ef3574c683fb8de95cb58d26c9da1e1cd24
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-11-12 12:41:38 +0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -10,37 +10,11 @@ import {
   showPanel,
 } from "./commands/scan";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "textlint-ai" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "textlint-ai.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from textLint-ai!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
-  const loginCommand = vscode.commands.registerCommand(
-    "textlint-ai.login",
-    async () => {
-      await promptApiKey();
-    }
-  );
-
-  context.subscriptions.push(loginCommand);
-
   const commands = [
+    { id: "textlint-ai.login", func: promptApiKey },
     { id: "textlint-ai.scanFile", func: scanFile },
     { id: "textlint-ai.scanSelection", func: scanSelection },
     { id: "textlint-ai.applyCorrection", func: applyCorrection },
@@ -54,5 +28,4 @@ export function activate(context: vscode.ExtensionContext) {
   });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
